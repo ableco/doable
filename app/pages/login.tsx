a9f2f31e501925244cbb278638b7ad961a3f4cfb
Julio García <julio.garcia@hey.com>
2021-07-21 17:08:26 -0500

    Use spacer

--- app/pages/login.tsx
+++ app/pages/login.tsx
@@ -2,9 +2,8 @@ import { Button } from "@ableco/abledev-components";
 
 function LoginPage() {
   return (
-    <div className="w-screen h-screen flex flex-col items-center justify-center">
+    <div className="w-screen h-screen flex flex-col items-center justify-center space-y-8">
       <h1 className="text-gray-900 font-bold text-3xl">Doable</h1>
-      <div className="h-8" />
       <a href="/api/auth/google">
         <Button variant="primary" size="base">
           Log in with Google
