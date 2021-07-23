03b9eee7e113ca76bbea9f75027a107650b1bf78
Julio García <julio.garcia@hey.com>
2021-07-23 15:14:32 -0500

    Add Center component

--- app/pages/login.tsx
+++ app/pages/login.tsx
@@ -1,13 +1,27 @@
 import { Button } from "@ableco/abledev-components";
+import { HTMLAttributes, ReactNode } from "react";
+
+interface CenterProps extends HTMLAttributes<HTMLDivElement> {
+  className?: string;
+}
+
+function Center({ className = "", ...props }: CenterProps) {
+  return (
+    <div
+      className={`flex items-center justify-center ${className}`}
+      {...props}
+    />
+  );
+}
 
 function LoginPage() {
   return (
-    <div className="w-screen h-screen flex flex-col items-center justify-center space-y-8">
+    <Center className="w-screen h-screen flex-col space-y-8">
       <h1 className="text-gray-900 font-bold text-3xl">Doable</h1>
       <a href="/api/auth/google">
         <Button>Log in with Google</Button>
       </a>
-    </div>
+    </Center>
   );
 }
 
