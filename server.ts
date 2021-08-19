dca9c4a9870e277f5ff3e033f2627749f51ffdfd
Julio García <julio.garcia@hey.com>
2021-08-19 00:58:01 -0500

    Use express as custom server and plug authentication

--- server.ts
+++ server.ts
@@ -1,39 +1,75 @@
-import blitz from "blitz/custom-server";
-import { createServer } from "http";
-import { parse } from "url";
+import { RequestContext } from "@ableco/abledev-dev-environment";
 import { log } from "@blitzjs/display";
-import { initIO, setupRoomsOnConnection } from "io";
+import { getSession } from "blitz";
+import blitz from "blitz/custom-server";
+import cookieParser from "cookie-parser";
+import db from "db";
 // In the future this line won't be needed because of
 // https://github.com/microsoft/TypeScript/issues/33079
 // @ts-ignore
 import { createHandleRequest } from "due-date/server-functions";
-import db from "db";
+import express from "express";
+import { createServer } from "http";
+import { initIO, setupRoomsOnConnection } from "io";
+import { parse } from "url";
+
+const handleAbledevRequest = createHandleRequest({
+  mode: "production",
+  hostContext: {
+    db,
+    authenticate: async (
+      request: RequestContext["request"],
+      response: RequestContext["response"],
+    ) => {
+      const session = await getSession(request, response);
+      if (session.userId) {
+        return { userId: session.userId };
+      } else {
+        response.status(401).send("Authentication Failure");
+        throw new Error("Authentication Failure");
+      }
+    },
+  },
+});
 
 const { PORT = "3000" } = process.env;
 const dev = process.env.NODE_ENV !== "production";
 const app = blitz({ dev });
-const handle = app.getRequestHandler();
+const blitzHandle = app.getRequestHandler();
 
-const handleAbledevRequest = createHandleRequest({
-  mode: "production",
-  hostContext: { db },
-});
+async function startServer() {
+  await app.prepare();
 
-app.prepare().then(() => {
-  const server = createServer((req, res) => {
-    const parsedUrl = parse(req.url!, true);
+  const expressServer = express();
+  expressServer.use(cookieParser());
+
+  expressServer.use(async (request, response) => {
+    const parsedUrl = parse(request.url!, true);
+
+    // This would probably be inside
     if (parsedUrl.pathname?.startsWith("/abledev")) {
-      handleAbledevRequest(req, res);
-    } else {
-      handle(req, res, parsedUrl);
+      // Blitz-specific trick to allow passing an anti-csrf header.
+      // Another alternative would have been to pass the header from abledev-react,
+      // but it would require a setup process in the frontend to configure special
+      // headers.
+      request.headers["anti-csrf"] =
+        request.cookies[`${global.sessionConfig.cookiePrefix}_sAntiCsrfToken`];
+
+      handleAbledevRequest(request, response);
     }
+
+    await blitzHandle(request, response, parsedUrl);
   });
+
+  const httpServer = createServer(expressServer);
 
   const io = initIO();
-  io.attach(server);
+  io.attach(httpServer);
   setupRoomsOnConnection(io);
 
-  server.listen(PORT, () => {
+  httpServer.listen(PORT, () => {
     log.success(`Ready on http://localhost:${PORT}`);
   });
-});
+}
+
+startServer();
